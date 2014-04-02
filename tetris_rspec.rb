b1b224adb2474e8d660d1051419b68d4c40b4a67
anto1nee <gely.antoine@gmail.com>
2014-04-02 13:09:59 +0200

    implement rotate90

--- tetris_rspec.rb
+++ tetris_rspec.rb
@@ -20,14 +20,13 @@ tab_2 = Array.new(4) {|i| Array.new(4) {|j| 0} }
 
 tab_2[1][1] = 1
 tab_2[2][1] = 1
+tab_2[2][2] = 1
 tab_2[2][3] = 1
-tab_2[2][4] = 1
 
 puts 'tab_2 (Etat final) :'
 print tab_2
 puts ''
 
 
-
 puts 'Result :'
-puts transform(tab_1).equal? tab_2 
+puts rotate90(tab_1).eql? (tab_2) 
